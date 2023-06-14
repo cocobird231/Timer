7f5977e3dc0872232e1d0577cd63483a3b41083c
coco-server <cocobird231@gmail.com>
2023-06-14 14:52:44 +0800

    Improve Timer cumulative error.

--- Timer.h
+++ Timer.h
@@ -10,7 +10,9 @@ using namespace std::chrono_literals;
 class Timer
 {
 private:
-    std::chrono::duration<float, std::milli> interval_;
+    std::chrono::high_resolution_clock::duration interval_;
+    std::chrono::high_resolution_clock::time_point st_;
+
     std::atomic<bool> activateF_;
     std::atomic<bool> exitF_;
     std::atomic<bool> funcCallableF_;
@@ -20,20 +22,20 @@ private:
     std::thread callbackTH_;
 
 private:
-    void _timer()
+    void _timer()// Deprecated
     {
-        while (!exitF_)
+        while (!this->exitF_)
         {
             try
             {
-                auto st = std::chrono::steady_clock::now();
-                while (!exitF_ && this->activateF_ && (std::chrono::steady_clock::now() - st < this->interval_))
+                auto st = std::chrono::high_resolution_clock::now();
+                while (!this->exitF_ && this->activateF_ && (std::chrono::high_resolution_clock::now() - st < this->interval_))
                     std::this_thread::yield();
-                if (!exitF_ && this->activateF_ && this->funcCallableF_)
+                if (!this->exitF_ && this->activateF_ && this->funcCallableF_)
                 {
                     if (this->callbackTH_.joinable())
                         this->callbackTH_.join();
-                    this->callbackTH_ = std::thread(&Timer::_timer_tick, this);
+                    this->callbackTH_ = std::thread(&Timer::_tick, this);
                 }
                 std::this_thread::yield();
             }
@@ -46,7 +48,38 @@ private:
             this->callbackTH_.join();
     }
 
-    void _timer_tick()
+    void _timer_fixedRate()
+    {
+        while (!this->exitF_)
+        {
+            try
+            {
+                if (!this->exitF_ && this->activateF_)
+                {
+                    auto tickTimePoint = this->st_ + this->interval_;
+                    this->st_ = tickTimePoint;
+
+                    while (!this->exitF_ && this->activateF_ && (std::chrono::high_resolution_clock::now() < tickTimePoint))
+                        std::this_thread::yield();
+                    if (!this->exitF_ && this->activateF_ && this->funcCallableF_)
+                    {
+                        if (this->callbackTH_.joinable())
+                            this->callbackTH_.join();
+                        this->callbackTH_ = std::thread(&Timer::_tick, this);
+                    }
+                }
+                std::this_thread::yield();
+            }
+            catch (const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+        }
+        if (this->callbackTH_.joinable())
+            this->callbackTH_.join();
+    }
+
+    void _tick()
     {
         this->funcCallableF_ = false;
         this->func_();
@@ -54,11 +87,12 @@ private:
     }
 
 public:
-    Timer(int interval_ms, const std::function<void()>& callback) : activateF_(false), exitF_(false), funcCallableF_(true)
+    Timer(double interval_ms, const std::function<void()>& callback) : activateF_(false), exitF_(false), funcCallableF_(true)
     {
-        this->interval_ = std::chrono::milliseconds(interval_ms);
+        this->interval_ = std::chrono::high_resolution_clock::duration(std::chrono::nanoseconds(static_cast<uint64_t>(interval_ms * 1000000)));
         this->func_ = callback;
-        this->timerTH_ = std::thread(&Timer::_timer, this);
+        this->st_ = std::chrono::high_resolution_clock::now();
+        this->timerTH_ = std::thread(&Timer::_timer_fixedRate, this);
     }
 
     ~Timer()
@@ -66,9 +100,20 @@ public:
         this->destroy();
     }
 
-    void start() { this->activateF_ = true; }
+    void start()
+    {
+        this->st_ = std::chrono::high_resolution_clock::now();
+        this->activateF_ = true;
+    }
 
     void stop() { this->activateF_ = false; }
+
+    void setInterval(double interval_ms)// Experimental
+    {
+        this->stop();
+        this->interval_ = std::chrono::high_resolution_clock::duration(std::chrono::nanoseconds(static_cast<uint64_t>(interval_ms * 1000000)));
+        this->start();
+    }
 
     void destroy()
     {
